d5011739a90c1bf36ed1f39df8cf4c71998931c5
Evan <evan.straw99@gmail.com>
2017-07-31 16:22:41 -0700

    create first version
    Bot can now respond to inline queries by showing a list of dice that it supports.
    Upon selecting one, a user will be presented with a message showing the number
    of sides on the die, the number that was rolled, and, if applicable, the number
    of times it was re-rolled. Each message has a re-roll button that can be pressed
    to roll the die again.
    
    Upon private-messaging the bot, it will simply send a D20 roll.

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ var exit = logutil.exit;
 
 var TelegramBot = require('node-telegram-bot-api');
 
+var uuid = require('uuid');
+
 var rl;
 var bot;
+
+var diceSizes = [4,6,8,10,12,20];
 
 checkArguments();
 var token = process.argv[2];
@@ -65,17 +69,94 @@ function startTelegramPolling(bot){
   function onMessage(msg)
   {
     log("Message from "+botBase.getUserFormat(msg.from)+": '"+msg.text+"'");
-    bot.sendMessage(msg.from.id,"Message received!");
+    var diceMsg = createDiceMessage(20);
+    bot.sendMessage(msg.from.id,diceMsg.text, {reply_markup:diceMsg.reply_markup});
   }
 
   function onInlineQuery(query)
   {
     log("Inline query from "+botBase.getUserFormat(query.from)+"; Query ID: "+query.id+"; Text: '"+query.query+"'");
+    var results = [];
+    for(var i in diceSizes)
+    {
+      var current = diceSizes[i];
+      var diceMsg = createDiceMessage(current);
+      results.push(createInlineQueryResult(uuid.v4(),'D'+current,current+'-sided die', diceMsg.text, diceMsg.reply_markup));
+    }
+    bot.answerInlineQuery(query.id, results);
   }
 
-function onCallbackQuery(query)
+  function onCallbackQuery(query)
+  {
+    log("Callback query from "+botBase.getUserFormat(query.from)+"; Query ID: "+query.id+"; Data: '"+query.data+"'");
+    var msgID;
+    var chatID;
+    var data = JSON.parse(query.data);
+    if(data.reroll)
+    {
+      var rerollCount = data.rerollCount;
+      if(!rerollCount){rerollCount = 0};
+      rerollCount++;
+      var sides = parseInt(data.reroll);
+      if(sides)
+      {
+        var newMsg = createDiceMessage(sides,rerollCount);
+        if(query.inline_message_id)
+        {
+          msgID = query.inline_message_id;
+          bot.editMessageText(newMsg.text, {inline_message_id:msgID,reply_markup:newMsg.reply_markup});
+        }
+        else
+        {
+          msgID = query.message.message_id;
+          chatID = query.message.chat.id;
+          bot.editMessageText(newMsg.text, {message_id:msgID,chat_id:chatID,reply_markup:newMsg.reply_markup});
+        }
+      }
+    }
+    bot.answerCallbackQuery(query.id,"",false,{});
+  }
+
+function createDiceMessage(sides,rerollCount)
 {
-  log("Callback query from "+botBase.getUserFormat(query.from)+"; Query ID: "+query.id+"; Data: '"+query.data+"'");
+  if(!rerollCount){rerollCount=0};
+  var msgText = "";
+  msgText += (sides+'-sided die (D'+sides+') roll:'+'\n');
+  msgText += roll(sides)+'\n';
+  if(rerollCount > 0)
+  {
+    msgText += "(Re-rolled "+rerollCount+" times)\n";
+  }
+
+  var buttons = [];
+  var firstRow = [];
+  firstRow.push(createInlineKeyboardButton("Roll Again",'{"reroll":'+sides+',"rerollCount":'+rerollCount+'}'));
+  buttons.push(firstRow);
+
+  var replyMarkup = createInlineKeyboardMarkup(buttons);
+  return {text:msgText,reply_markup:replyMarkup};
+}
+
+function roll(sides)
+{
+  return Math.floor(Math.random()*sides)+1;
+}
+
+
+//requires a two-dimensional array of inline keyboard button objects and wraps them in the syntax that telegram requires.
+//see createInlineKeyboardButton for a function that generates them.
+function createInlineKeyboardMarkup(buttons){
+  return {inline_keyboard:buttons};
+}
+
+function createInlineKeyboardButton(text, callbackData){
+  return {text:text,callback_data:callbackData};
+}
+
+//abstraction
+//returns an inline query result object given the id and some information.
+function createInlineQueryResult(resultId, title, description, content, replyMarkup){
+  return {type:"article",id:resultId,title:title,description:description,input_message_content:{message_text:content}, reply_markup:replyMarkup};
 }
 
  function handleConsoleCommand(command)
@@ -99,6 +180,26 @@ function onCallbackQuery(query)
           message += " ";
         }
         bot.sendMessage(id,message);
+      }
+    }
+    if(args[0] == "roll")
+    {
+      if(args.length < 2)
+      {
+        log("Usage: roll <sides>",levels.err);
+      }
+      else
+      {
+        var sides = parseInt(args[1]);
+        if(sides)
+        {
+          var result = roll(sides);
+          log(sides+"-sided die roll: "+result);
+        }
+        else
+        {
+          log("Sides must be a valid number",levels.err);
+        }
       }
     }
   }
